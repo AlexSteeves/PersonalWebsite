7364c861216c19e7f204a3da5950808d4d5c346d
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2022-08-09 16:26:59 -0300

    Commit 1

---
+++ my-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+
+
+    return (
+        <div className = "App">
+            <h1>Hello</h1>
+            <Navbar />
+        </div>
+    );
+}
+
+export default App
